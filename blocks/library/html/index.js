51b4ab90557485d84eafa8665d2f4553c270f931
Robert Anderson <robert@noisysocks.com>
2018-01-18 14:51:43 +1100

    Improve HTML block cursor behaviour
    - Permit pressing ESCAPE to unfocus the currently selected block.
    - Allow pressing UP/DOWN to move focus to the previous/next block when
      the cursor is at the start/end of the editor.

--- blocks/library/html/index.js
+++ blocks/library/html/index.js
@@ -39,7 +39,7 @@ export const settings = {
 
 	edit: withState( {
 		preview: false,
-	} )( ( { attributes, setAttributes, setState, isSelected, preview } ) => (
+	} )( ( { attributes, setAttributes, setState, isSelected, toggleSelection, preview } ) => (
 		<div className="wp-block-html">
 			{ isSelected && (
 				<BlockControls>
@@ -64,7 +64,8 @@ export const settings = {
 			) : (
 				<CodeEditor
 					value={ attributes.content }
-					focus={ !! focus }
+					focus={ isSelected }
+					onFocus={ toggleSelection }
 					onChange={ content => setAttributes( { content } ) }
 				/>
 			) }
