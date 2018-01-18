62453a9fe3a51891df28188ea748bec7c64d42da
Adam Silverstein <adam@10up.com>
2018-01-18 09:01:53 -0500

    set up the autosave on load so the first autosave fires correctly

--- editor/store/effects.js
+++ editor/store/effects.js
@@ -40,6 +40,7 @@ import {
 	updateReusableBlock,
 	saveReusableBlock,
 	insertBlock,
+	resetAutosave,
 } from './actions';
 import {
 	getCurrentPost,
@@ -346,6 +347,7 @@ export default {
 		// Resetting post should occur after blocks have been reset, since it's
 		// the post reset that restarts history (used in dirty detection).
 		effects.push( resetPost( post ) );
+		effects.push( resetAutosave( post ) );
 
 		// Include auto draft title in edits while not flagging post as dirty
 		if ( post.status === 'auto-draft' ) {
