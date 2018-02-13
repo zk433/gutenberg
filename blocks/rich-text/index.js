bafd49b4eddbe9ae9c54209ae1551d86f514464d
iseulde <wp@iseulde.com>
2018-02-13 11:59:28 +0100

    Setting content is no longer needed on split if state is in sync

--- blocks/rich-text/index.js
+++ blocks/rich-text/index.js
@@ -559,7 +559,6 @@ export class RichText extends Component {
 				const beforeElement = nodeListToReact( beforeNodes, createTinyMCEElement );
 				const afterElement = nodeListToReact( afterNodes, createTinyMCEElement );
 
-				this.setContent( beforeElement );
 				this.props.onSplit( beforeElement, afterElement );
 			} else {
 				event.preventDefault();
@@ -618,10 +617,8 @@ export class RichText extends Component {
 			const beforeElement = nodeListToReact( beforeFragment.childNodes, createTinyMCEElement );
 			const afterElement = isLinkBoundary( afterFragment ) ? [] : nodeListToReact( afterFragment.childNodes, createTinyMCEElement );
 
-			this.setContent( beforeElement );
 			this.props.onSplit( beforeElement, afterElement, ...blocks );
 		} else {
-			this.setContent( [] );
 			this.props.onSplit( [], [], ...blocks );
 		}
 	}
